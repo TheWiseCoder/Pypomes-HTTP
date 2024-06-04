51e4c905b3ae2cc2582ca65b3f02e748b3b0b736
GT Nunes <wisecoder01@gmail.com>
2024-06-03 23:55:08 -0300

    Release 0.1.9

--- src/pypomes_http/http_async.py
+++ src/pypomes_http/http_async.py
@@ -14,7 +14,7 @@ class HttpAsync(threading.Thread):
     """
     Asynchronous invocation of a *REST* service.
 
-    This invocation is done with *request* and the method specified in *job_method*.
+    This invocation is done with Python's *request* and the method specified in *job_method*.
     """
 
     def __init__(self, job_name:
@@ -36,20 +36,20 @@ class HttpAsync(threading.Thread):
         This is the structure of the results sent:
 
         {
-            "job_name": "<str>"             -- the name given for the job
+            "job-name": "<str>"            -- the name given for the job
 
-            "start": "<iso-date>",          -- timestamp of invocation start (ISO format)
+            "start": "<iso-date>",         -- timestamp of invocation start (ISO format)
 
-            "finish": "<iso-date>",         -- timestamp of invocation finish (ISO format)
+            "finish": "<iso-date>",        -- timestamp of invocation finish (ISO format)
 
-            "errors": "<errors-reported>"   -- errors returned by the service, if applicable
+            "errors": "<errors-reported>"  -- errors returned by the service, if applicable
 
-            "response": "<bytes-in-BASE64>" -- Base64-wrapped contents of the response
+            "content": "<bytes-in-BASE64>" -- Base64-wrapped contents of the response
         }
 
-        :param job_name: name of the job being invoked
+        :param job_name: the name of the job being invoked
         :param job_url: the job's URL
-        :param job_method: the REST method to use (GET, POST or PUT)
+        :param job_method: the REST method to use (DELETE, GET, PATCH, POST, PUT)
         :param callback: the function to call on job termination
         :param report_content: whether to report the response's content to callback
         :param headers: optional headers
@@ -105,7 +105,7 @@ class HttpAsync(threading.Thread):
                                         timeout=self.timeout,
                                         logger=self.logger)
 
-        # obtain the finisg timestamp
+        # obtain the finish timestamp
         self.finish_timestamp = datetime.now(TIMEZONE_LOCAL).isoformat()
 
         if self.logger:
@@ -115,19 +115,23 @@ class HttpAsync(threading.Thread):
         if self.callback:
             # yes, send it the results of the service invocation
             reply: dict = {
-                "job_name": self.job_name,
+                "job-name": self.job_name,
                 "start": self.start_timestamp,
                 "finish": self.finish_timestamp,
             }
 
             # any errors ?
-            if len(errors) > 0:
+            if errors:
                 # yes, report the errors messages
                 reply["errors"] = json.dumps(errors, ensure_ascii=False)
-            elif self.report_content and \
-                    response is not None and \
-                    isinstance(response.content, bytes):
-                reply["response"] = base64.b64encode(response.content).decode()
 
-            # send message to recipient
-            self.callback(reply)
+            # return the response's content, if appropriate
+            if (self.report_content
+                and response and
+                hasattr(response, "content") and
+                isinstance(response.content, bytes)):
+                reply["content"] = base64.b64encode(response.content).decode()
+
+            # send message to recipient, if applicable
+            if self.callback:
+                self.callback(reply)
